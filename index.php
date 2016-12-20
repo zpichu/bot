8668c68afae41482cb7f2cbcf2f2af4da47d6933
zpichu <zpichu@gmail.com>
2016-12-20 16:28:16 +0800

    add

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
+<?php 
+
+$bottoken = "284722676:AAGkLhEL16o56ctCGITjz7AuywKC-GhIues";
+$website = "https://api.telegram.org/bot".$bottoken;
+
+$update = file_get_contents("php://input");
+
+$updateArray = json_decode($update, TRUE);
+
+$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
+
+?>
